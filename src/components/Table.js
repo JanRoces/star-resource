d6fb8b511966da12ce9db3c1127a155e7ada5f46
JanRoces <55074172+JanRoces@users.noreply.github.com>
2023-10-06 00:42:57 -0700

    render table rows

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,5 @@
 import "../styles/Table.css";
+import { STAR_SYSTEMS } from "../utils/systems";
 
 function formatName(str) {
   return str.replace(/\w\S*/g, (txt) => {
@@ -8,6 +9,10 @@ function formatName(str) {
 
 function Table() {
   const headerConfig = [
+    {
+      label: "level",
+      flex: "1",
+    },
     {
       label: "system",
       flex: "1",
@@ -26,7 +31,7 @@ function Table() {
     },
     {
       label: "resources",
-      flex: "2",
+      flex: "3",
     },
   ];
 
@@ -48,7 +53,100 @@ function Table() {
     return <div className="header-container">{headerLabels}</div>;
   }
 
-  return <div className="table-container">{renderHeader()}</div>;
+  function renderRows() {
+    const planetType = {
+      moon: "Moon",
+      planet: "Planet",
+    };
+
+    const rows = [];
+
+    let alternate = true;
+
+    STAR_SYSTEMS.forEach((system) => {
+      system.planets.forEach((planet) => {
+        alternate = !alternate;
+
+        rows.push(
+          renderRow({
+            level: system.level,
+            systemName: system.name,
+            planetName: planet.name,
+            type: planetType.planet,
+            mainPlanet: "",
+            resources: planet.resources,
+            alternateColor: alternate,
+          })
+        );
+        planet.moons.forEach((moon) => {
+          alternate = !alternate;
+
+          rows.push(
+            renderRow({
+              level: system.level,
+              systemName: system.name,
+              planetName: moon.name,
+              type: planetType.moon,
+              mainPlanet: planet.name,
+              resources: moon.resources,
+              alternateColor: alternate,
+            })
+          );
+        });
+      });
+    });
+
+    return rows;
+  }
+
+  function renderRow({
+    level,
+    systemName,
+    planetName,
+    type,
+    mainPlanet,
+    resources,
+    alternateColor,
+  }) {
+    const resourcesSymbols = resources.map((r) => r.symbol);
+    const symbolList = resourcesSymbols.join(", ");
+
+    const color = alternateColor ? "#2E4A76" : "#486389";
+
+    return (
+      <div
+        className="row-container"
+        key={systemName + "-" + planetName}
+        style={{ backgroundColor: color }}
+      >
+        <div className="column-container">
+          <span>{level}</span>
+        </div>
+        <div className="column-container">
+          <span>{formatName(systemName)}</span>
+        </div>
+        <div className="column-container">
+          <span>{formatName(planetName)}</span>
+        </div>
+        <div className="column-container">
+          <span>{type}</span>
+        </div>
+        <div className="column-container">
+          <span>{formatName(mainPlanet)}</span>
+        </div>
+        <div className="column-container" style={{ flex: 3 }}>
+          <span>{symbolList}</span>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="table-container">
+      {renderHeader()}
+      {renderRows()}
+    </div>
+  );
 }
 
 export default Table;
