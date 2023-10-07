12d9d7dd753655cfd2a91507f0db9453174a2ed2
JanRoces <55074172+JanRoces@users.noreply.github.com>
2023-10-06 17:38:07 -0700

    create resource component

---
+++ src/components/Resource.js
@@ -0,0 +1,30 @@
+import "../styles/Resource.css";
+
+function Resource({ symbol, rarity, backgroundColor, size }) {
+  function renderRarityIcon() {
+    const color = rarity.color || backgroundColor;
+
+    return (
+      <span
+        className="dot"
+        style={{
+          backgroundColor: color,
+        }}
+      ></span>
+    );
+  }
+
+  return (
+    <div>
+      <div
+        className="resource-container"
+        style={{ backgroundColor: backgroundColor }}
+      >
+        <div className="rarity">{renderRarityIcon()}</div>
+        <div className="symbol">{symbol}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Resource;
